c1d8e3d9fc51c0b95f6aface7b493163dfc51455
Jonny Stoten <jonny.stoten@docker.com>
2024-06-20 14:24:48 +0100

    Use prod TUF

--- pkg/handler/tuf.go
+++ pkg/handler/tuf.go
@@ -7,11 +7,11 @@ import (
 
 func createTufClient(outputPath string) (*tuf.TufClient, error) {
 	// using oci tuf metadata and targets
-	// metadataURI := "registry-1.docker.io/docker/tuf-metadata:latest"
-	// targetsURI := "registry-1.docker.io/docker/tuf-targets"
+	metadataURI := "registry-1.docker.io/docker/tuf-metadata:latest"
+	targetsURI := "registry-1.docker.io/docker/tuf-targets"
 	// example using http tuf metadata and targets
-	metadataURI := "https://docker.github.io/tuf-staging/metadata"
-	targetsURI := "https://docker.github.io/tuf-staging/targets"
+	// metadataURI := "https://docker.github.io/tuf-staging/metadata"
+	// targetsURI := "https://docker.github.io/tuf-staging/targets"
 
 	return tuf.NewTufClient(embed.StagingRoot, outputPath, metadataURI, targetsURI, tuf.NewVersionChecker())
 }
